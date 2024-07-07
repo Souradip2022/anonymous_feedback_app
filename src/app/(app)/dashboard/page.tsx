c55541c5673b58f87c650a7a91acd37d16210850
Souradip2022 <22052939@kiit.ac.in>
2024-07-08 01:28:29 +0530

    Completed final message dashboard design

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -13,6 +13,7 @@ import {useRouter} from "next/navigation";
 import axios, {AxiosError} from "axios";
 import {ApiResponseHandler} from "@/utils/ApiResponseHandler";
 import {RiLoader3Fill} from "react-icons/ri";
+import {ImCross} from "react-icons/im";
 
 function Page() {
   const router = useRouter();
@@ -28,61 +29,14 @@ function Page() {
   });
   const acceptMessages = watch("acceptMessages");
 
-  useEffect(() => {
-    if (status !== "authenticated") {
-      return;
-    }
-
-    if (typeof window !== 'undefined' && session?.user?.username) {
-      setUserUrl(`${window.location.protocol}//${window.location.host}/u/${session.user.username}`);
-    }
-
-    (async function () {
-      try {
-        const response = await axios.get(`/api/accept-message`);
-
-        setValue("acceptMessages", response.data.data.userAcceptingMessage);
-        // console.log(response.data.data.userAcceptingMessage);
-      } catch (error: any) {
-        const axiosError = error as AxiosError<ApiResponseHandler>;
-        toast({
-          title: "Failed to fetch user message status",
-          description: axiosError.response?.data.message,
-          variant: "destructive"
-        })
-      }
-    })()
-
-    fetchMessage();
-
-  }, [session, status, toast, setValue]);
-
-  const acceptMessagesHandler = useCallback(async () => {
-    if (status !== "authenticated") {
-      return;
-    }
-
-    try {
-      setIsSwitchLoading(true);
-      const response = await axios.post<ApiResponseHandler>(`/api/accept-message`, {
-        acceptMessages: !acceptMessages
-      });
-
-      toast({
-        description: response.data.message
-      });
-    } catch (error: any) {
-      const axiosError = error as AxiosError<ApiResponseHandler>;
-
-      toast({
-        title: axiosError.response?.data.message,
-        variant: "destructive",
-      });
-    } finally {
-      setIsSwitchLoading(false);
-      setValue("acceptMessages", !acceptMessages);
-    }
-  }, [acceptMessages, setValue, toast, status]);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(userUrl);
+    toast({
+      title: 'URL Copied!',
+      variant: "default",
+      description: 'Profile URL has been copied to clipboard.',
+    });
+  };
 
   const fetchMessage = useCallback(async () => {
     if (status !== "authenticated") {
@@ -118,18 +72,67 @@ function Page() {
 
   }, [status, toast]);
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(userUrl);
-    toast({
-      title: 'URL Copied!',
-      variant: "default",
-      description: 'Profile URL has been copied to clipboard.',
-    });
-  };
+  useEffect(() => {
+    if (status !== "authenticated") {
+      return;
+    }
+    // console.log("use effect invoked")
+    if (typeof window !== 'undefined' && session?.user?.username) {
+      setUserUrl(`${window.location.protocol}//${window.location.host}/u/${session.user.username}`);
+    }
+
+    (async function () {
+      try {
+        const response = await axios.get(`/api/accept-message`);
+
+        setValue("acceptMessages", response.data.data.userAcceptingMessage);
+        // console.log(response.data.data.userAcceptingMessage);
+      } catch (error: any) {
+        const axiosError = error as AxiosError<ApiResponseHandler>;
+        toast({
+          title: "Failed to fetch user message status",
+          description: axiosError.response?.data.message,
+          variant: "destructive"
+        })
+      }
+    })()
+
+    fetchMessage();
+
+  }, [session, status, toast, setValue, fetchMessage]);
+
+
+  const acceptMessagesHandler = useCallback(async () => {
+    if (status !== "authenticated") {
+      return;
+    }
+
+    try {
+      setIsSwitchLoading(true);
+      const response = await axios.post<ApiResponseHandler>(`/api/accept-message`, {
+        acceptMessages: !acceptMessages
+      });
+
+      toast({
+        description: response.data.message
+      });
+    } catch (error: any) {
+      const axiosError = error as AxiosError<ApiResponseHandler>;
+
+      toast({
+        title: axiosError.response?.data.message,
+        variant: "destructive",
+      });
+    } finally {
+      setIsSwitchLoading(false);
+      setValue("acceptMessages", !acceptMessages);
+    }
+  }, [acceptMessages, setValue, toast, status]);
+
 
   return (
-    <div className="w-full h-screen bg-secondary-foreground flex flex-col items-center justify-center px-40">
-      <h1 className="text-4xl font-bold text-secondary self-start">User Dashboard</h1>
+    <div className="w-full h-fit bg-secondary-foreground flex flex-col items-center justify-center px-40">
+      <h1 className="text-4xl font-bold text-secondary self-start mt-24">User Dashboard</h1>
       <h2 className={"text-xl font-semibold text-secondary self-start py-2.5"}>Your Username</h2>
       <div className="w-full flex items-center justify-between ">
         <p className={"text-secondary"}>{userUrl}</p>
@@ -162,10 +165,35 @@ function Page() {
       {messages.length === 0 ? (
         <div className="text-secondary my-5">No Message</div>
       ) : (
-        <div className="text-secondary my-5">
-          {messages.map((message) => (
-            <div key={message._id}>{message.content}</div>
-          ))}
+        <div className="text-secondary my-5 grid grid-cols-2 gap-10 place-items-center">
+          {messages.map((message) => {
+            const isoString = `${message.createdAt}`;
+            const date = new Date(isoString);
+
+            function formatDate(date: any) {
+              const options = {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+              };
+              return date.toLocaleString(undefined, options);
+            }
+
+            const readableDate = formatDate(date);
+
+            return (
+              <div key={message._id}
+                   className={"w-full h-fit p-5 border border-muted-foreground rounded-md shadow-md flex flex-col items-center justify-around gap-y-5 relative"}>
+                <h2 className={"self-start"}>{readableDate}</h2>
+                <p className={"self-start"}>{message.content}</p>
+                <div className={"bg-red-600 p-2.5 rounded-md hover:bg-red-400 absolute top-3 right-3"}>
+                  <ImCross color={"white"}/>
+                </div>
+              </div>)
+          })}
         </div>
       )}
     </div>
--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,61 +1,62 @@
-import { dbConnect } from "@/lib/dbConnect";
-import { UserModel } from "@/model/UserModel";
-import { ApiResponseHandler } from "@/utils/ApiResponseHandler";
-import { MessageSchema } from "@/schema/MessageSchema";
+import {dbConnect} from "@/lib/dbConnect";
+import {UserModel} from "@/model/UserModel";
+import {ApiResponseHandler} from "@/utils/ApiResponseHandler";
+import {MessageSchema} from "@/schema/MessageSchema";
+import {Message} from "@/model/UserModel"
 
 export async function POST(request: Request): Promise<Response> {
   await dbConnect();
 
   try {
-    const { username, content } = await request.json();
-    console.table([ username, content]);
+    const {username, content} = await request.json();
+    console.table([username, content]);
 
     // Validate content using Zod schema
-    const verifyContent = { content };
+    const verifyContent = {content};
     const result = MessageSchema.safeParse(verifyContent);
 
     if (!result.success) {
       const error = result.error.format().content?._errors;
       console.log("Validation error:", error);
-      return new Response(
-        JSON.stringify(new ApiResponseHandler(false, "Error parsing content", { error })),
-        { status: 400 }
+      return Response.json(
+        new ApiResponseHandler(false, "Error parsing content", {error}),
+        {status: 400}
       );
     }
 
-    const user = await UserModel.findOne({ username });
+    const user = await UserModel.findOne({username});
 
     if (!user) {
       console.log("User not found:", username);
-      return new Response(
-        JSON.stringify(new ApiResponseHandler(false, "User not found", {})),
-        { status: 404 }
+      return Response.json(
+        new ApiResponseHandler(false, "User not found", {}),
+        {status: 404}
       );
     }
 
     if (!user.isAcceptingMessage) {
       console.log("User is not accepting messages:", username);
-      return new Response(
-        JSON.stringify(new ApiResponseHandler(false, "User is not accepting messages", {})),
-        { status: 403 }
+      return Response.json(
+        new ApiResponseHandler(false, "User is not accepting messages", {}),
+        {status: 403}
       );
     }
 
     // Create a new message using the MessageSchema
-    const newMessage = { content, createdAt: new Date(Date.now()) };
-    user.messages.push(newMessage);
+    const newMessage = {content, createdAt: new Date(Date.now())};
+    user.messages.push(newMessage as Message);
     await user.save();
 
     console.log("Message saved successfully:", newMessage);
-    return new Response(
-      JSON.stringify(new ApiResponseHandler(true, "New message saved successfully", { newMessage })),
-      { status: 200 }
+    return Response.json(
+      new ApiResponseHandler(true, "New message saved successfully", {newMessage}),
+      {status: 200}
     );
   } catch (error: unknown) {
     console.log("Unexpected error occurred:", error);
-    return new Response(
-      JSON.stringify(new ApiResponseHandler(false, "Internal server error", { error })),
-      { status: 500 }
-    );
+    return Response.json(
+      new ApiResponseHandler(false, "Internal server error", {error}),
+      {status: 500}
+    )
   }
 }
