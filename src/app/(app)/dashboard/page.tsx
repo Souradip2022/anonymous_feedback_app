a7f71915c8104d22af598220486831a990fb2ce7
souradipdev <22052939@kiit.ac.in>
2024-08-04 02:41:28 +0530

    Added Speech reading of text messages

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -9,7 +9,6 @@ import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
 import {useSession} from "next-auth/react";
 import {useToast} from "@/components/ui/use-toast";
-import {useRouter} from "next/navigation";
 import axios, {AxiosError} from "axios";
 import {ApiResponseHandler} from "@/utils/ApiResponseHandler";
 import {RiLoader3Fill} from "react-icons/ri";
@@ -22,15 +21,24 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {HiMiniSpeakerXMark, HiOutlineSpeakerWave} from "react-icons/hi2";
+import {IoPauseOutline} from "react-icons/io5";
+import {FaPlay} from "react-icons/fa";
+
 
 function Page() {
-  const router = useRouter();
   const {toast} = useToast();
   const {data: session, status} = useSession();
   const [userUrl, setUserUrl] = useState<string>('');
   const [isSwitchLoading, setIsSwitchLoading] = useState<boolean>(false);
-  const [isLoadingMessage, setIsLoadingMessage] = useState<boolean>(false)
+  const [isLoadingMessage, setIsLoadingMessage] = useState<boolean>(false);
   const [messages, setMessages] = useState<any[]>([]);
+  // console.log(messages);
+  const [isSpeaking, setIsSpeaking] = useState<any>({
+    speaking: false,
+    index: null
+  });
+  const [pauseAudio, setPauseAudio] = useState<boolean>(false);
 
   const {register, watch, setValue} = useForm({
     resolver: zodResolver(AcceptMessageSchema)
@@ -159,6 +167,95 @@ function Page() {
     }
   }
 
+  function setVoiceAndSpeak(utterance: any, index: number) {
+    const voices = window.speechSynthesis.getVoices();
+    // console.log('Available voices:', voices);
+
+    const usEnglishVoice = voices.find(voice => voice.lang === 'en-US' && voice.name === 'Google US English');
+
+    utterance.onend = () => {
+      setIsSpeaking({
+        speaking: false,
+        index: null
+      });
+    }
+
+    if (usEnglishVoice) {
+      utterance.voice = usEnglishVoice;
+      // console.log('Selected voice:', usEnglishVoice);
+    } else {
+      // console.log('US English voice not found');
+    }
+
+    // Speak the utterance
+    window.speechSynthesis.speak(utterance);
+    /*if (speechSynthesis.speaking) {
+      setIsSpeaking({
+        speaking: true,
+        index
+      });
+    }*/
+    // console.log('Utterance spoken:', utterance);
+  }
+
+  function speakAudio(index: number) {
+    const text = messages[index].content;
+    if (text && 'speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance();
+      utterance.text = text;
+      utterance.lang = "en-US";
+
+      // If voices are already loaded, speak immediately
+      if (window.speechSynthesis.getVoices().length > 0) {
+        setVoiceAndSpeak(utterance, index);
+      } else {
+        // Otherwise, wait for the voices to be loaded
+        window.speechSynthesis.onvoiceschanged = () => setVoiceAndSpeak(utterance, index);
+      }
+    }
+  }
+
+  function abortAudio() {
+    if ('speechSynthesis' in window) {
+      window.speechSynthesis.cancel();
+      setIsSpeaking({
+        speaking: false,
+        index: null
+      });
+    }
+  }
+
+  useEffect(() => {
+    if ('speechSynthesis' in window) {
+      if (pauseAudio) {
+        window.speechSynthesis.pause();
+      } else {
+        window.speechSynthesis.resume();
+      }
+    }
+  }, [pauseAudio]);
+
+  useEffect(() => {
+    const index = isSpeaking.index;
+    if (isSpeaking.speaking === true && index !== null && index !== undefined) {
+      // console.log(isSpeaking.index)
+      speakAudio(index);
+      if(!isSpeaking.speaking && isSpeaking.index === null){
+        return;
+      }
+    } else if (index !== null && index !== undefined) {
+      abortAudio();
+
+      if(!isSpeaking.speaking && isSpeaking.index === null){
+        return;
+      }
+    }
+
+    console.log(isSpeaking);
+  }, [isSpeaking]);
+
+
+
   return (
     <div
       className="w-full min-h-screen max-h-fit bg-secondary-foreground flex flex-col items-center pt-28">
@@ -218,15 +315,43 @@ function Page() {
             return (
               <div
                 key={message._id}
-                className={"w-full h-full p-5 border border-muted-foreground rounded-md shadow-md flex flex-col items-center justify-around gap-y-5 relative "}>
-                <h2 className={"self-start"}>{readableDate}</h2>
+                className={"w-full h-full p-5 border border-muted-foreground rounded-md shadow-md flex flex-col items-center gap-y-5 relative "}>
+                <div className={"self-start flex align-middle justify-center gap-x-3.5"}>
+                  <h2 className={"h-full grid place-items-center"}>{readableDate}</h2>
+                  <span className={"flex items-center gap-x-3.5"}>
+                    {isSpeaking.speaking ?
+                      <>
+                        <Button variant={"default"} className={"hover:bg-gray-100 px-1 py-0.5"} onClick={() => setIsSpeaking({
+                          speaking: false,
+                          index
+                        })}>
+                          <HiOutlineSpeakerWave/>
+                        </Button>
+                        <Button variant={"default"} className={"hover:bg-gray-100 px-1 py-0.5"}
+                                onClick={() => setPauseAudio(prev => !prev)}>
+                          {pauseAudio ? <FaPlay/> : <IoPauseOutline/>}
+                        </Button>
+                      </> :
+                      <>
+                        <Button variant={"default"} className={"hover:bg-gray-100 px-1 py-0.5"} onClick={() => setIsSpeaking({
+                          speaking: true,
+                          index
+                        })}>
+                          <HiMiniSpeakerXMark/>
+                        </Button>
+                      </>
+
+                    }
+                  </span>
+                </div>
+
                 <p className={"self-start w-full break-words "}>{message.content}</p>
 
                 <Dialog>
                   <DialogTrigger asChild>
                     <button type={"button"}
                             className={"bg-red-600 p-2.5 rounded-md hover:bg-red-400 absolute top-3 right-3 h-8 w-8"}
-                            /*onClick={() => deleteMessage(index)}*/>
+                    >
                       <ImCross color={"white"} className={"h-full w-full"}/>
                     </button>
                   </DialogTrigger>
